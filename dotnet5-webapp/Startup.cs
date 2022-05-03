3ef956185264a62c03d8c642b15c298b3870e130
Vinicius Nascimento <viniciusnp.1998@gmail.com>
2022-05-03 10:00:37 -0300

    Create DataContext, ConnectionString & Adding the DbContext

--- dotnet5-webapp/Startup.cs
+++ dotnet5-webapp/Startup.cs
@@ -1,6 +1,8 @@
+using dotnet5_webapp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,7 @@ namespace dotnet5_webapp
         {
 
             services.AddControllers();
+            services.AddDbContext<DataContext>(db => db.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet5_webapp", Version = "v1" });
